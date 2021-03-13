611aece08f9dd7ec059ead354ae0095a402398b5
Trang <10674637@uvu.edu>
2021-03-12 20:28:46 -0700

    creating front page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,101 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <main>
+    <div class="jumbotron custom-bg-dark">
+
+      <div class="card bg-dark text-white">
+      <img src="../assets/img1.jpg" class="card-img img-responsive" alt="...">
+      
+      <div class="card-img-overlay">
+        <h1 class="card-title" style="text-shadow:5px 5px 10px red">Did you know?</h1>
+        <p class="card-text" style="font-size:15px; font-family: Verdana, serif;">Every day, more than 1,700 dogs and cats are killed in our nation’s shelters because they don’t have safe places to call home. But people like you are changing that.</p>
+      </div>
+    </div>
+      <!-- <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="src/assets/cat.jpg" width="200px" height="430px" class="d-block w-100" alt="">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>This will be the first message</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="src/assets/ausiedog.jpg" class="d-block w-100" alt="">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="src/assets/pig.jpg" class="d-block w-100" alt="">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel"  data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+
+    </div> -->
+
   </div>
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/SA_LifeLearn.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title" style="color:black;" >Lost & Found</h5>
+          <p style="color:black; text-shadow: yellow">Click to view all found pets or Login to post a lost or found a pet.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/ShowImage.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title" style="color:black;" >Donation</h5>
+          <p style="color:black;">Click to donate to our shelter.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card h-100">
+        <img src="../assets/cat2.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title" style="color:black;" >Adoption</h5>
+          <p style="color:black;">Click to view all available pets for adoption. Please log in to send in your application or contact us via phone.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+<br>
+  <div class="jumbotron custom-bg-dark">
+    <h1 class="display-4">Contact Information</h1>
+    <hr class="my-4">
+    <p>
+      Click below to begin managing task for users.
+    </p>
+    <router-link class="btn btn-sucess btn-lg" to="/tasks">
+      Adoption
+    </router-link>
+  </div>
+
+  
+
+</main>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,99 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top custom-bg-dark">
+            <router-link to="/" class="navbar-brand">
+                <img style="max-height:50px;"
+                src="../assets/logoanimal.png"/>Utah Animal Shelter
+            </router-link>
+
+            <button class="navbar-toggler" 
+            type="button" data-toggle="collapse" 
+            data-target="#navbarCollapse" 
+            aria-controls="navbarCollapse"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link" exact>
+                            Home
+                    </router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/adoption" class="nav-link" exact>
+                            Adoption
+                    </router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/donation" class="nav-link" exact>
+                            Donation
+                    </router-link>
+                </li>
+
+                <li v-if="$store.state.isLoggedIn" class="nav-item">
+                    <router-link to="/tasks" class="nav-link" exact>
+                            Tasks
+                    </router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/lost&found" class="nav-link" exact>
+                            Lost & Found
+                    </router-link>
+                </li>
+
+                <li v-if="!$store.state.isLoggedIn" class="nav-item">
+                    <router-link to="/register" class="nav-link" exact>
+                            Register
+                    </router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/contact" class="nav-link" exact>
+                            Contact
+                    </router-link>
+                </li>
+
+                <li v-if="!$store.state.isLoggedIn" class="nav-item">
+                    <router-link to="/login" class="nav-link" exact>
+                            Login
+                    </router-link>
+                </li>
+
+                <li v-if="$store.state.isLoggedIn" class="nav-item">
+                    <a @click.prevent="logout()" class="nav-link" href="#">
+                            Logout
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                            {{this.$store.state.username ? this.$store.state.username: 'User'}}
+
+                    </a>
+                </li>
+
+            </ul>
+
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+// import * as auth from '../services/AuthService'
+
+export default {
+    name: 'Navbar',
+    methods: {
+        logout: function() {
+            // auth.logout();
+            this.$router.push({name: 'Home'});
+        }
+    }
+}
+</script>
